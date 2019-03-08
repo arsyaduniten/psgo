d7814857e1e2824ca135b695d039e6b0def1a39d
Azad Arsyad Zainuddin <azad@policystreet.com>
2019-03-08 18:06:19 +0800

    latest

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -30,8 +30,8 @@ class PlanController extends Controller
 	    $response = $client->request('GET', 'https://humn27zyud.execute-api.ap-southeast-1.amazonaws.com/latest?id=1_hJK5K2iJMFwYNmE0Dyo5Vd5zzQAJvkK74nl1Byq4KE&sheet=4');
 	    $response = $response->getBody()->getContents();
 	    $allname = json_decode($response, true);
-	    dd($allpremiums);
     	$country_index = array_search($request->get('destination'),$allcountries['columns']['countries']);
+    	$available_plans = $available_plans_name = [];
     	if(!$country_index){
     	} else {
     		$allianz_reg = $allcountries['columns']['allianz'][$country_index];
@@ -42,9 +42,27 @@ class PlanController extends Controller
 	    	$sompo_reg = $allcountries['columns']['sompo'][$country_index];
 	    	$amg_reg = $allcountries['columns']['amg'][$country_index];
 
-	    	$atc_plan = "atcx".config('constants.'.$allianz_reg).'adultnsc';
-	    	$aig_plan = "aigx".config('constants.'.$allianz_reg).'adultnsc';
-
+	    	$available_plans[] = "atcx".config('constants.'.$allianz_reg).'adultnsc';
+			$available_plans[] = "atc2x".config('constants.'.$allianz_reg).'adultnsc';
+			$available_plans[] = "aigx".config('constants.'.$aig_reg).'adultnsc';
+			$available_plans[] = "aigp1".config('constants.'.$aig_reg).'adultnsc';
+			$available_plans[] = "aigp2".config('constants.'.$aig_reg).'adultnsc';
+			$available_plans[] = "aigp3".config('constants.'.$aig_reg).'adultnsc';
+			$available_plans[] = "axap1".config('constants.'.$axa_reg).'adultnsc';
+			$available_plans[] = "axap2".config('constants.'.$axa_reg).'adultnsc';
+			$available_plans[] = "axap3".config('constants.'.$axa_reg).'adultnsc';
+			$available_plans[] = "axap4".config('constants.'.$axa_reg).'adultnsc';
+			$available_plans[] = "axap5".config('constants.'.$axa_reg).'adultnsc';
+			$available_plans[] = "tunex".config('constants.'.$tune_reg).'adultnsc';
+			$available_plans[] = "bsx".config('constants.'.$sompo_reg).'adultnsc';
+			$available_plans[] = "bsp1".config('constants.'.$sompo_reg).'adultnsc';
+			$available_plans[] = "bsp2".config('constants.'.$sompo_reg).'adultnsc';
+			$available_plans[] = "kurniap1".config('constants.'.$amg_reg).'adultnsc';
+			$available_plans[] = "kurniap2".config('constants.'.$amg_reg).'adultnsc';
+			$available_plans[] = "kurniap3".config('constants.'.$amg_reg).'adultnsc';
+			$available_plans[] = "kurniap4".config('constants.'.$amg_reg).'adultnsc';
     	}
+
+    	return view('public.plans');
     }
 }
